3af42915b7ff729abce58f600ebc217c2f9d26c5
RobynAwesome <rkholofelo@gmail.com>
2026-03-18 12:16:16 +0200

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 
-const MainLayout = () => {
+export default function MainLayout() {
   return (
-    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+    <div>
+      <header>
+        <h1>My Blog</h1>
+        {/* ✅ Use the same name you imported */}
         <Navbar />
+      </header>
+
+      <main>
+        {/* ✅ This is where child routes render */}
         <Outlet />
+      </main>
     </div>
   );
-};
-
-export default MainLayout;
+}
