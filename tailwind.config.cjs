881759628ebe4182b3efc7cd81855425876c27c3
RobynAwesome <rkholofelo@gmail.com>
2026-03-18 07:36:31 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+/** * Tailwind Configuration
+ * Author: https://www.linkedin.com/in/kholofelo-robyn-rababalela-7a26273b6
+ * @type {import('tailwindcss').Config} 
+ */
+export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: { extend: {} },
   plugins: [],
