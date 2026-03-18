b7dada8acbc06f54cf3fe9b810df11ab7d99b5fe
RobynAwesome <rkholofelo@gmail.com>
2026-03-18 07:36:26 +0200

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,4 +1,7 @@
-module.exports = {
+/** * PostCSS Configuration
+ * Author: https://www.linkedin.com/in/kholofelo-robyn-rababalela-7a26273b6
+ */
+export default {
   plugins: {
     '@tailwindcss/postcss': {},
     autoprefixer: {},
