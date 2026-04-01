8d23a8c8eb309daf2d609744b8b2797f21503c42
Kholofelo Robyn Rababalela <rkholofelo@gmail.com>
2026-04-01 18:07:20 +0200

    Fix @ alias path for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -15,7 +16,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "./src"),
     },
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
   },
